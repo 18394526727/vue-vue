5c9acd8994336d0a16c4be2ef112f08b5350fda3
18394526727 <1197265493@qq.com>
2019-05-06 07:44:19 +0800

    feat

---
+++ src/components/Animation/GroupList.vue
@@ -0,0 +1,51 @@
+<template>
+  <transition-group name="list" mode="out-in" tag="div" class="span-group">
+    <!--v-on:before-enter="beforeEnter"
+    v-on:after-enter="afterEnter"-->
+    <span class="span" v-for="item in list" :key="item">{{ item }}</span>
+  </transition-group>
+</template>
+<script>
+export default {
+  props: {
+    list: {
+      type: String
+    }
+  },
+  methods: {
+    /* beforeEnter: function(el) {
+      // ...
+    },
+    afterEnter: function(el) {
+      // ...
+    } */
+  }
+};
+</script>
+<style lang="scss" scoped>
+.span-group {
+  color: red;
+  border: 1px solid red;
+  height: auto;
+  display: flex;
+  flex-wrap: wrap;
+  .span {
+    margin: 0 10px;
+  }
+}
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.4s;
+}
+.list-enter {
+  opacity: 0;
+  transform: translateX(-30px);
+  color: blue;
+}
+.list-leave-to {
+  color: black;
+  opacity: 0;
+  font-weight: 900;
+  transform: translate(50px, -50px);
+}
+</style>
--- src/views/Animation.vue
+++ src/views/Animation.vue
@@ -1,29 +1,50 @@
 <template>
   <div>
-    <dynamic-num :number="number"></dynamic-num>
+    <dynamic-num class="number" :number="number"></dynamic-num>
+    <div class="buttons">
+      <button class="increase" @click="add">+</button>
+      <button class="sub" @click="sub">-</button>
+    </div>
+    <group-list :list="list"></group-list>
   </div>
 </template>
 <script>
-import { setInterval } from "timers";
 export default {
   data() {
     return {
-      number: 0
+      number: 0,
+      list: []
     };
   },
   mounted() {
-    this.add();
+    this.increase();
   },
   methods: {
-    add() {
-      let timer = setInterval(() => {
-        this.number += 100;
-        if (this.number === 10000) {
+    increase() {
+      let timer = window.setInterval(() => {
+        this.number += 200;
+        if (this.number === 1000) {
           window.clearInterval(timer);
+          timer = null;
         }
       }, 3000);
+    },
+    add() {
+      this.list.push((Math.random() * 100).toFixed(2));
+    },
+    sub() {
+      this.list.splice(this.randomIndex(), 1);
+    },
+    randomIndex: function() {
+      return Math.floor(Math.random() * this.list.length);
     }
   }
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.number {
+  font-size: 30px;
+  color: blue;
+  filter: brightness(80);
+}
+</style>
